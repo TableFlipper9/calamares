df7ed2cc08d282631e5fb8f0731e3a371dbb0232
TableFlipper9 <hithack9@gmail.com>
2025-08-01 12:00:16 +0300

    Calamares 3.3.14: introducing Gentoo Stage3 Chooser and respective modules
    * introduced stage3 choosing module
    * created boxes to choose mirrors, arches and stage3 archives
    * introduced simple warning to new user regarding
      blank mirror, which means default mirror
    * error showing for faild fetches or network problems
    
    Signed-off-by: Morovan Mihai <mihaimorovan1@gmail.com>

---
+++ src/modules/stagechoose/StageFetcher.cpp
@@ -0,0 +1,152 @@
+#include "StageFetcher.h"
+
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QEventLoop>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
+#include <QRegularExpressionMatchIterator>
+#include <QStringList>
+
+StageFetcher :: StageFetcher(QObject* parent):QObject(parent)
+{
+}
+
+QString StageFetcher::extractvariantBase(const QString& variant){
+    if(variant.startsWith("current-"))
+        return variant.mid(8);
+    return variant;
+}
+
+void StageFetcher::setMirrorBase(const QString& mirror)
+{
+    QString base = mirror.trimmed();
+    while(base.endsWith('/')) base.chop(1);
+
+    if(base.isEmpty())
+        base = QStringLiteral("http://distfiles.gentoo.org/releases");
+
+    if(!base.endsWith("/releases"))
+        base += "/releases";
+
+    m_mirrorBase = base;
+}
+
+void StageFetcher::cancelOngoingRequest()
+{
+    if(m_currentReply){
+        disconnect(m_currentReply,nullptr,this,nullptr);
+        if(m_currentReply->isRunning())
+            m_currentReply->abort();
+        m_currentReply->deleteLater();
+        m_currentReply = nullptr;
+    }
+}
+
+void StageFetcher::fetchVariants(const QString& arch)
+{
+    cancelOngoingRequest(); 
+    emit fetchStatusChanged("Fetching variants for " + arch + "...");
+
+    QString urlStr = QString("%1/%2/autobuilds/").arg(m_mirrorBase, arch);
+    QUrl url(urlStr);
+    QNetworkRequest request(url);
+
+    QNetworkReply* reply = m_nam.get(request);
+    m_currentReply = reply;
+    connect(reply, &QNetworkReply::finished, this,[this, reply](){onVariantsReplyFinished(reply);});
+}
+
+void StageFetcher::onVariantsReplyFinished(QNetworkReply* reply)
+{
+    if(!reply)
+        return;
+
+    if(reply != m_currentReply){
+        reply->deleteLater();
+        return;
+    }
+
+    QStringList variants;
+    if(reply->error() != QNetworkReply::NoError){
+        emit fetchError(reply->errorString());
+        reply->deleteLater();
+        if(m_currentReply == reply) m_currentReply = nullptr;
+        return;
+    }
+
+    QString html = reply->readAll();
+     if(html.isEmpty())
+        emit variantsFetched(variants);
+
+    QRegularExpression re(R"((current-stage3-[^"/]+)[/])");
+    QRegularExpressionMatchIterator iterator = re.globalMatch(html);
+
+    QStringList seen;
+    while(iterator.hasNext()){
+        QRegularExpressionMatch match = iterator.next();
+        QString variant = match.captured(1);
+        if(!seen.contains(variant)){
+            variants.append(variant);
+            seen.append(variant);
+        }
+    }
+
+    emit variantsFetched(variants);
+    emit fetchStatusChanged("Idle");
+    reply->deleteLater();
+    if(reply == m_currentReply) m_currentReply = nullptr;
+}
+
+void StageFetcher::fetchLatestTarball(const QString& arch, const QString& variant)
+{
+    cancelOngoingRequest();
+    emit fetchStatusChanged("Fetching Tarball for "+ variant +"...");
+    const QString baseUrl = QString("%1/%2/autobuilds/%3/").arg(m_mirrorBase, arch, variant);
+    QUrl url(baseUrl);
+    QNetworkRequest request(url);
+
+    QNetworkReply* reply = m_nam.get(request);
+    m_currentReply = reply;
+    connect(reply, &QNetworkReply::finished, this, [this, reply, variant](){onTarballReplyFinished(reply, variant);});
+}
+
+void StageFetcher::onTarballReplyFinished(QNetworkReply* reply, const QString& variant)
+{
+    if(!reply)
+        return;
+
+    if(reply != m_currentReply){
+        reply->deleteLater();
+        return;
+    }
+
+    QString latest;
+    if(reply->error() != QNetworkReply::NoError){
+        emit fetchError(reply->errorString());
+        reply->deleteLater();
+        if(m_currentReply == reply) m_currentReply = nullptr;
+        return;
+    }
+
+    QString html = reply->readAll();
+    if(html.isEmpty())
+        emit tarballFetched(latest);
+
+    QRegularExpression re(QString("(%1-[\\dTZ]+\\.tar\\.xz)").arg(StageFetcher::extractvariantBase(variant)));
+    QRegularExpressionMatchIterator iterator = re.globalMatch(html);
+
+    while(iterator.hasNext()){
+        QRegularExpressionMatch match = iterator.next();
+        QString filename = match.captured(1);
+        if(filename > latest){
+            latest = filename;
+        }
+    }
+
+    emit tarballFetched(latest);
+    emit fetchStatusChanged("Idle");
+    reply->deleteLater();
+    if(reply == m_currentReply) m_currentReply = nullptr;
+}
