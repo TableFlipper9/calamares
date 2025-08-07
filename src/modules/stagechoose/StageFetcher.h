66e066e97a7c5dd59cc590e684239b53eb3e2e46
TableFlipper9 <hithack9@gmail.com>
2025-08-06 23:25:01 -0400

    StageFetcher integration and test

--- src/modules/stagechoose/StageFetcher.h
+++ src/modules/stagechoose/StageFetcher.h
@@ -3,17 +3,15 @@
 
 #include <QObject>
 #include <QStringList>
+#include <QUrl>
 
 class StageFetcher : public QObject
 {
     Q_OBJECT
 
 public: 
-    explicit StageFetcher(QObject* parent = nullptr);
-
     static QStringList fetchVariants(const QString& arch);
 
-private:
     static QString fetchHtml(const QUrl& url);
 };
 
