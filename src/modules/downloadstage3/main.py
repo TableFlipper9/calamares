02d5770e7b21551a8d30bea38ed3d579310cb88a
Stefan Cristian B. <stefan.cristian+git@rogentos.ro>
2026-02-12 03:04:24 +0200

    src/modules: disable removal of var/db/repos/gentoo in downloadstage3, set webrsync to none for gentoopkg, we do not need it there

--- src/modules/downloadstage3/main.py
+++ src/modules/downloadstage3/main.py
@@ -385,11 +385,6 @@ def run():
                 elif os.path.isdir(entry):
                     shutil.rmtree(entry)
 
-    gentoo_repo = os.path.join(extract_path, "var/db/repos/gentoo")
-    if os.path.exists(gentoo_repo):
-        shutil.rmtree(gentoo_repo)
-        print("Removed /var/db/repos/gentoo to free RAM")
-
     return None
 
 def write_dracut_config(root_mount_point, stage_name_tar):
